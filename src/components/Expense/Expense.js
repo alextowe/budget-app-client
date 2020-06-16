6bdae9af1dbe809509825d831df4d9ff89e91480
Alex Towe <66729163+alextowe@users.noreply.github.com>
2020-06-15 22:49:00 -0400

    basic styles for progressbar

--- src/components/Expense/Expense.js
+++ src/components/Expense/Expense.js
@@ -3,24 +3,15 @@ import ProgressBar from '../ProgressBar/ProgressBar'
 import './Expense.css'
 
 export default class Expense extends Component {
-  renderAmountSection = () => {
-    const { item } = this.props
-    return (
-      <section className='amount_section'>
-        <p className='amount'>
-          ${ item.expected_amount === null ? '0' : item.expected_amount}
-        </p>
-        <ProgressBar />
-      </section>
-    )
-  }
-
   render() {
     const { expense } = this.props
     return (
       <li className='Expense'>
         <p className='Expense__name'>{expense.name}</p>
-        <ProgressBar />
+        <ProgressBar 
+          expected={expense.expected_amount}
+          actual={expense.actual_amount}
+        />
       </li>
     )
   }
--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,9 +5,13 @@ import './ProgressBar.css'
 
 export default class ProgressBar extends Component {
   render() {
+    const { expected, actual } = this.props
+    console.log(expected, actual)
     return (
       <Section className='ProgressBar'>
-        <div className='ProgressBar_progress' />
+        <div className='ProgressBar__progress'>
+
+        </div>
       </Section>
     )
   }
